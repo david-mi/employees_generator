81b40b420d69fb851fec811d860738537dde9d0d
David Michel <david.michel2809@gmail.com>
2023-04-08 15:04:46 +0200

    feat: add possibility to rename employee properties on generateEmployees

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -1,24 +1,57 @@
-import { getRandomElementFromArray } from "./utils.js";
+import { getRandomElementFromArray as generateEmployee } from "./utils.js";
 import { names, departments, states, streets, cities, zipCodes } from "./data/index.js";
 
 interface Employee {
   [key: string]: string | number
 }
 
-function generateEmployees(amount: number = 1): Employee[] {
+type KeyNames = {
+  firstName?: string
+  lastName?: string,
+  department?: string,
+  state?: string,
+  street?: string,
+  city?: string,
+  zipCode?: number
+}
+
+/**
+ * Generate a list of employees
+ * 
+ * @param {number} [amount=1] - amount of employees to generate
+ * @param keyNames - rename a key from {@link Employee} object
+ * @example 
+ * {
+ *  firstName: "firstname",
+ *  city: "RANDOM_CITY"
+ * }
+ * // should return 
+ * {
+ *   firstname: string
+ *   lastName: string,
+ *   department: string,
+ *   state: string,
+ *   street: string,
+ *   RANDOM_CITY: string,
+ *   zipCode: number
+ * }
+ */
+
+
+function generateEmployees(amount: number = 1, keyNames: KeyNames = {}): Employee[] {
   const employees: Employee[] = []
   let count = 0;
   let firstName, lastName, department, state, street, city, zipCode
 
   while (count < amount) {
     employees.push({
-      firstName: getRandomElementFromArray(names, firstName),
-      lastName: getRandomElementFromArray(names, lastName),
-      department: getRandomElementFromArray(departments, department),
-      state: getRandomElementFromArray(states, state),
-      street: getRandomElementFromArray(streets, street),
-      city: getRandomElementFromArray(cities, city),
-      zipCode: getRandomElementFromArray(zipCodes, zipCode)
+      [keyNames.firstName || "firstName"]: generateEmployee(names, firstName),
+      [keyNames.lastName || "lastName"]: generateEmployee(names, lastName),
+      [keyNames.department || "department"]: generateEmployee(departments, department),
+      [keyNames.state || "state"]: generateEmployee(states, state),
+      [keyNames.street || "street"]: generateEmployee(streets, street),
+      [keyNames.city || "city"]: generateEmployee(cities, city),
+      [keyNames.zipCode || "zipCode"]: generateEmployee(zipCodes, zipCode)
     })
 
     count++

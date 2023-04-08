af81c573916fb992695ac1de2084f2451830ec2d
David Michel <david.michel2809@gmail.com>
2023-04-08 14:34:23 +0200

    refacto: refacto generateEmployees

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -8,31 +8,17 @@ interface Employee {
 function generateEmployees(amount: number = 1): Employee[] {
   const employees: Employee[] = []
   let count = 0;
-  let firstName = ""
-  let lastName = ""
-  let department = ""
-  let state = ""
-  let street = ""
-  let city = ""
-  let zipCode = 0
+  let firstName, lastName, department, state, street, city, zipCode
 
   while (count < amount) {
-    firstName = getRandomElementFromArray(names, firstName)
-    lastName = getRandomElementFromArray(names, lastName)
-    department = getRandomElementFromArray(departments, department)
-    state = getRandomElementFromArray(states, state)
-    street = getRandomElementFromArray(streets, street)
-    city = getRandomElementFromArray(cities, city)
-    zipCode = getRandomElementFromArray(zipCodes, zipCode)
-
     employees.push({
-      firstName,
-      lastName,
-      department,
-      street,
-      city,
-      state,
-      zipCode
+      firstName: getRandomElementFromArray(names, firstName),
+      lastName: getRandomElementFromArray(names, lastName),
+      department: getRandomElementFromArray(departments, department),
+      state: getRandomElementFromArray(states, state),
+      street: getRandomElementFromArray(streets, street),
+      city: getRandomElementFromArray(cities, city),
+      zipCode: getRandomElementFromArray(zipCodes, zipCode)
     })
 
     count++
